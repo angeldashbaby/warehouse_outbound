0fb91832b37263e2ddb61d1a31f6e74882a22a12
Ratchathorn <ratchathorn.s@ku.th>
2022-11-01 13:35:10 +0700

    first commit

---
+++ src/main/java/angelbaby/inbound/controller/InboundController.java
@@ -0,0 +1,17 @@
+package angelbaby.inbound.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class InboundController {
+    String payload;
+
+
+
+
+
+}
